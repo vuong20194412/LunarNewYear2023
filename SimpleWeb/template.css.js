206e9054e621fd1eb78b70a6064c8b353337577a
vuong <vuong.thcodeforces194412@outlook.com.vn>
2023-01-25 22:49:52 +0700

    Revert "7 project"
    This reverts commit 03aa36a7e913c8fc8074d592d43ae3ef038279e0.

--- SimpleWeb/template.css.js
+++
@@ -1,8 +0,0 @@
-style = `
-.layout-center{height:100vh;width:100vw;display:grid;place-items:center;}
-`;
-
-node = document.createElement("style");
-node.appendChild(document.createTextNode(style));
-// Append the "style" node to the list:
-document.getElementById('head').appendChild(node);
